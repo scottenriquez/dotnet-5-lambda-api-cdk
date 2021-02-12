465b501ab3443bb6084edd7e11f40f284ae6597c
Scottie Enriquez <scottie@enriquez.xxx>
2021-02-11 19:58:04 -0600

    Adding CDK skeleton

---
+++ LambdaApiSolution/src/LambdaApiSolution/LambdaApiSolutionStack.cs
@@ -0,0 +1,12 @@
+using Amazon.CDK;
+
+namespace LambdaApiSolution
+{
+    public class LambdaApiSolutionStack : Stack
+    {
+        internal LambdaApiSolutionStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
