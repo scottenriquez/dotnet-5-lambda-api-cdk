631a3b5e166c1fa394906cd6206962cf20f2f20b
Scottie Enriquez <scottie@enriquez.xxx>
2021-02-18 19:25:09 -0600

    Updating CDK version

--- LambdaApiSolution/src/LambdaApiSolution.DockerFunction/test/LambdaApiSolution.DockerFunction.Tests/FunctionTest.cs
+++ LambdaApiSolution/src/LambdaApiSolution.DockerFunction/test/LambdaApiSolution.DockerFunction.Tests/FunctionTest.cs
@@ -1,13 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Amazon.Lambda.APIGatewayEvents;
 using Xunit;
-using Amazon.Lambda.Core;
 using Amazon.Lambda.TestUtilities;
-
-using LambdaApiSolution.DockerFunction;
 
 namespace LambdaApiSolution.DockerFunction.Tests
 {
